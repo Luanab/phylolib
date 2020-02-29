5fdd620fcf0659a9f9c6d826042acd5c4540dc7e
Luanab <luana.b.silva@hotmail.com>
2020-02-29 17:07:48 +0000

    Remove wrong verification from Component run(). Add ComponentTest class.

--- PhyloLib/src/main/java/flow/Component.java
+++ PhyloLib/src/main/java/flow/Component.java
@@ -28,21 +28,18 @@ public abstract class Component<T> {
 
 	public static <T extends Component<?>> void run(Arguments arguments, Context context, String command, boolean single, HashMap<String, IConstructor<T>> constructors)
 			throws RepeatedCommandException, InvalidTypeException, InvalidFileException, InvalidFormatException, MissingInputException, IOException {
-		if (arguments.containsKey(command)) {
-			List<Parameters> commands = arguments.get(command);
-			if (single && commands.size() > 1)
-				throw new RepeatedCommandException(command);
-			for (Parameters parameters : commands) {
-				String type = parameters.getType();
-				IConstructor<T> constructor = constructors.get(type);
-				if (constructor == null)
-					throw new InvalidTypeException(command, type);
-				Logger.info("Command '" + command + "' with type '" + type + "' started running");
-				constructor.construct(context, parameters.getOptions()).run();
-				Logger.info("Command '" + command + "' with type '" + type + "' finished running");
-			}
-		} else
-			Logger.warning("Ignoring invalid command '" + command + "'");
+		List<Parameters> commands = arguments.get(command);
+		if (single && commands.size() > 1)
+			throw new RepeatedCommandException(command);
+		for (Parameters parameters : commands) {
+			String type = parameters.getType();
+			IConstructor<T> constructor = constructors.get(type);
+			if (constructor == null)
+				throw new InvalidTypeException(command, type);
+			Logger.info("Command '" + command + "' with type '" + type + "' started running");
+			constructor.construct(context, parameters.getOptions()).run();
+			Logger.info("Command '" + command + "' with type '" + type + "' finished running");
+		}
 	}
 
 	protected abstract T process();
