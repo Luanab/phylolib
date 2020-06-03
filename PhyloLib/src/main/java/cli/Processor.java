a9c9449cf6bde4a5ca01c98d25efb7ce250fdc0a
Luanab <luana.b.silva@hotmail.com>
2020-06-03 14:10:48 +0100

    Previous commit continuation.

--- PhyloLib/src/main/java/cli/Processor.java
+++ PhyloLib/src/main/java/cli/Processor.java
@@ -1,8 +1,8 @@
 package cli;
 
-import data.dataset.IDatasetProcessor;
-import data.matrix.IMatrixProcessor;
-import data.tree.ITreeProcessor;
+import data.dataset.DatasetProcessor;
+import data.matrix.MatrixProcessor;
+import data.tree.TreeProcessor;
 import reflection.Reflection;
 
 import java.lang.reflect.Constructor;
@@ -10,9 +10,9 @@ import java.util.Map;
 
 public enum Processor {
 
-	DATASET(Option.DATASET, IDatasetProcessor.class),
-	MATRIX(Option.MATRIX, IMatrixProcessor.class),
-	TREE(Option.TREE, ITreeProcessor.class);
+	DATASET(Option.DATASET, DatasetProcessor.class),
+	MATRIX(Option.MATRIX, MatrixProcessor.class),
+	TREE(Option.TREE, TreeProcessor.class);
 
 	private final Option option;
 	private final Class<?> processor;
