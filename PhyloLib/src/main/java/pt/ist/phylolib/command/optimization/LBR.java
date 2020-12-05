df7b6170dae55d3adbed6434920ca284a6428431
Luana Silva <luana.silva@hb-itconsulting.pt>
2020-12-05 18:34:46 +0000

    Generalize local optimization.

--- PhyloLib/src/main/java/pt/ist/phylolib/command/optimization/LBR.java
+++ PhyloLib/src/main/java/pt/ist/phylolib/command/optimization/LBR.java
@@ -1,14 +1,10 @@
 package pt.ist.phylolib.command.optimization;
 
-import pt.ist.phylolib.cli.Options;
-import pt.ist.phylolib.data.Context;
-import pt.ist.phylolib.data.dataset.Dataset;
 import pt.ist.phylolib.data.matrix.Matrix;
 import pt.ist.phylolib.data.tree.Edge;
 import pt.ist.phylolib.data.tree.Tree;
-import pt.ist.phylolib.exception.MissingInputException;
 
-import java.util.*;
+import java.util.Collection;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -18,42 +14,14 @@ import java.util.stream.Stream;
  */
 public final class LBR extends Optimization {
 
-	private int loci;
-	private String[] ids;
-	private Matrix matrix;
-
 	@Override
-	public void init(Context context, Options options) throws MissingInputException {
-		Dataset dataset = context.getDataset(options);
-		this.loci = dataset.profile(0).size();
-		this.ids = dataset.ids();
-		this.matrix = context.getMatrix(options);
+	protected Edge join(int loci, Matrix matrix, Tree tree, Edge edge) {
+		int w = recraft(loci, matrix, tree, edge.from(), edge.to());
+		int v = recraft(loci, matrix, tree, edge.to(), edge.from());
+		return new Edge(w, v, matrix.distance(w, v));
 	}
 
-	@Override
-	public final Tree process(Tree tree) {
-		Tree result = new Tree(ids);
-		PriorityQueue<Edge> edges = new PriorityQueue<>(Comparator.comparingDouble(Edge::distance));
-		Map<Integer, Integer> map = new HashMap<>(ids.length);
-		for (int i = 0; i < tree.ids().length; i++) {
-			String id = tree.ids()[i];
-			map.put(i, IntStream.range(0, ids.length).filter(j -> ids[j].equals(id)).findFirst().orElseThrow());
-		}
-		tree.edges().map(e -> new Edge(map.get(e.from()), map.get(e.to()), e.distance())).peek(result::add).forEach(edges::add);
-		while (!edges.isEmpty()) {
-			Edge previous = edges.remove();
-			result.remove(previous);
-			int u = recraft(result, previous.from(), previous.to());
-			int v = recraft(result, previous.to(), previous.from());
-			Edge current = new Edge(u, v, matrix.distance(u, v));
-			result.add(current);
-			if (current.distance() > previous.distance())
-				edges.add(current);
-		}
-		return result;
-	}
-
-	private int recraft(Tree tree, int u, int v) {
+	private int recraft(int loci, Matrix matrix, Tree tree, int u, int v) {
 		return tree.edges()
 				.map(Edge::from)
 				.filter(i -> tree.edges().noneMatch(edge -> edge.to() == i))
@@ -62,41 +30,41 @@ public final class LBR extends Optimization {
 				.flatMap(Collection::stream)
 				.distinct()
 				.filter(n -> !n.equals(u))
-				.reduce(u, (uu, w) -> select(uu, w, v));
+				.reduce(u, (uu, w) -> select(loci, matrix, uu, w, v));
 	}
 
 	private Stream<Integer> children(Tree tree, int i) {
 		return Stream.concat(Stream.of(i), tree.edges().filter(edge -> edge.from() == i).flatMap(edge -> children(tree, edge.to())));
 	}
 
-	private int select(int u, int w, int v) {
+	private int select(int loci, Matrix matrix, int u, int w, int v) {
 		double uw = matrix.distance(u, w);
 		double wu = matrix.distance(w, u);
 		double uv = matrix.distance(u, v);
 		double wv = matrix.distance(w, v);
-		double a = contemporary(uw, uv, wv);
-		double b = ancestor(wu, uv, wv);
-		return (a >= b && mean(u) > mean(w)) || (a < b && uv > wv) ? w : u;
+		double a = contemporary(loci, uw, uv, wv);
+		double b = ancestor(loci, wu, uv, wv);
+		return (a >= b && mean(matrix, u) > mean(matrix, w)) || (a < b && uv > wv) ? w : u;
 	}
 
-	private double contemporary(double uw, double uv, double wv) {
+	private double contemporary(int loci, double uw, double uv, double wv) {
 		double l = Math.sqrt(1 - uw);
-		return likelihood(uw, uv, wv, l, (1 - (uv + wv) / 2) / l, l);
+		return likelihood(loci, uw, uv, wv, l, (1 - (uv + wv) / 2) / l, l);
 	}
 
-	private double ancestor(double wu, double uv, double wv) {
+	private double ancestor(int loci, double wu, double uv, double wv) {
 		double x = 1 - ((1 - wu) * (1 - wv) + (1 - uv)) / 2;
 		double y = uv - 2 * x;
-		return likelihood(wu, uv, wv, 1 + (x * wu) / y, 1 + (x * wv) / y, 1);
+		return likelihood(loci, wu, uv, wv, 1 + (x * wu) / y, 1 + (x * wv) / y, 1);
 	}
 
-	private double likelihood(double d, double uv, double wv, double l, double k, double y) {
+	private double likelihood(int loci, double d, double uv, double wv, double l, double k, double y) {
 		return loci * d * Math.log(1 - y * l) + loci * (1 - d) * Math.log(y * l)
 		       + loci * uv * Math.log(1 - l * k) + loci * (1 - uv) * Math.log(l * k)
 		       + loci * wv * Math.log(1 - y * k) + loci * (1 - wv) * Math.log(y * k);
 	}
 
-	private double mean(int i) {
+	private double mean(Matrix matrix, int i) {
 		return (matrix.size() - 1) / IntStream.range(0, matrix.size()).filter(j -> j != i).mapToDouble(j -> 1 / matrix.distance(i, j)).sum();
 	}
 
--- PhyloLib/src/main/java/pt/ist/phylolib/command/optimization/Optimization.java
+++ PhyloLib/src/main/java/pt/ist/phylolib/command/optimization/Optimization.java
@@ -1,11 +1,70 @@
 package pt.ist.phylolib.command.optimization;
 
+import pt.ist.phylolib.cli.Options;
 import pt.ist.phylolib.command.ICommand;
+import pt.ist.phylolib.data.Context;
+import pt.ist.phylolib.data.dataset.Dataset;
+import pt.ist.phylolib.data.matrix.Matrix;
+import pt.ist.phylolib.data.tree.Edge;
 import pt.ist.phylolib.data.tree.Tree;
+import pt.ist.phylolib.exception.MissingInputException;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 /**
  * Responsible for optimizing a {@link Tree phylogenetic tree} into another.
  */
 public abstract class Optimization implements ICommand<Tree, Tree> {
+
+	private int loci;
+	private String[] ids;
+	private Matrix matrix;
+	private PriorityQueue<Edge> edges;
+
+	@Override
+	public void init(Context context, Options options) throws MissingInputException {
+		Dataset dataset = context.getDataset(options);
+		this.loci = dataset.profile(0).size();
+		this.ids = dataset.ids();
+		this.matrix = context.getMatrix(options);
+		this.edges = new PriorityQueue<>(Comparator.comparingDouble(Edge::distance));
+	}
+
+	@Override
+	public final Tree process(Tree tree) {
+		Tree result = new Tree(ids);
+		Map<Integer, Integer> map = new HashMap<>(ids.length);
+		for (int i = 0; i < tree.ids().length; i++) {
+			String id = tree.ids()[i];
+			map.put(i, IntStream.range(0, ids.length).filter(j -> ids[j].equals(id)).findFirst().orElseThrow());
+		}
+		tree.edges().map(e -> new Edge(map.get(e.from()), map.get(e.to()), e.distance())).peek(result::add).forEach(edges::add);
+		while (!edges.isEmpty()) {
+			Edge previous = edges.peek();
+			result.remove(previous);
+			Edge current = join(loci, matrix, result, previous);
+			edges.remove();
+			result.add(current);
+			if (current.distance() > previous.distance())
+				edges.add(current);
+		}
+		return result;
+	}
+
+	/**
+	 * Gets an edge that connects the two clusters of a given tree.
+	 *
+	 * @param loci   the number of loci of each profile
+	 * @param matrix the distance matrix with the distances between each profile
+	 * @param tree   the phylogenetic tree with two clusters
+	 * @param edge   the previous edge that connected the tree
+	 *
+	 * @return an edge that connects the tree
+	 */
+	protected abstract Edge join(int loci, Matrix matrix, Tree tree, Edge edge);
 
 }
